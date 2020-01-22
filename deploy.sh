8af1f5287c69a5b45c562e2474093ba2cc51bf7d
al7262 <alta5@alta5>
2020-01-22 09:49:51 +0700

    save

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id_rsa &&
+cd /var/www/backend
+git pull
+
+source ~/.profile
+echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+docker stop portfolio-api
+docker rm portfolio-api
+docker rmi al7262/portfolio:backend
+docker run -d --name portfolio-api -p 5000:5000 al7262/portfolio:backend
