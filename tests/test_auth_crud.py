eb71c9b53560e03b6a6efc5c98c8175d7b0bcbc0
al7262 <alta5@alta5>
2020-01-23 22:34:23 +0700

    save

--- tests/test_auth_crud.py
+++ tests/test_auth_crud.py
@@ -4,55 +4,21 @@ from flask import Flask, request, json
 import hashlib
 
 class TestAuthCrud():
-    def test_get_internal(client):
+    def test_get_admin(client):
         req = call_client(request)
         data = {
-            'client_key': 'internal',
-            'client_secret': 'Int3rn4l$ecr3t'
+            'username': 'admin',
+            'password': 'admin123'
         }
-        res = req.get('/auth', query_string=data)
+        res = req.get('/login', query_string=data)
         assert res.status_code==200
 
-    def test_get_normal(client):
+    def test_get_user(client):
         reset_db()
         req = call_client(request)
         data = {
-            'client_key': 'NUMBER01',
-            'client_secret': 'FIRST01'
+            'username': 'al7262',
+            'password': 'Alt3rr4'
         }
-        res = req.get('/auth', query_string=data)
-        assert res.status_code==200
-    
-    def test_get_invalid(client):
-        reset_db()
-        req = call_client(request)
-        data = {
-            'client_key': 'nnmnm',
-            'client_secret': 'FIRST01'
-        }
-        res = req.get('/auth', query_string=data)
-        assert res.status_code==401
-
-    def test_post_internal(client):
-        token = create_token(True)
-        req = call_client(request)
-        res = req.post('/auth', headers={'Authorization': 'Bearer ' + token})
-        assert res.status_code==200
-    
-    def test_post_normal(client):
-        token = create_token()
-        req = call_client(request)
-        res = req.post('/auth', headers={'Authorization': 'Bearer ' + token})
-        assert res.status_code==200
-
-    def test_refresh_internal(client):
-        token = create_token(True)
-        req = call_client(request)
-        res = req.post('/auth/refresh', headers={'Authorization': 'Bearer ' + token})
-        assert res.status_code==200
-    
-    def test_refresh_normal(client):
-        token = create_token()
-        req = call_client(request)
-        res = req.post('/auth/refresh', headers={'Authorization': 'Bearer ' + token})
+        res = req.get('/login', query_string=data)
         assert res.status_code==200
