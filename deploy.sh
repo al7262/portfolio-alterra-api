25498c42cc9320280d82a2c4ece66d893e7a4de7
al7262 <alta5@alta5>
2020-01-24 13:37:14 +0700

    save

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdi
 docker stop portfolio-api
 docker rm portfolio-api
 docker rmi al7262/portfolio:backend
-docker run -d --name portfolio-api -p 5000:5000 al7262/portfolio:backend
+docker run -d --name portfolio-api -p 5000:5000 al7262/portfolio:latest
